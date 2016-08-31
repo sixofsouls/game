572263fea85dc04c125efd27a6b19ac9cdac30b2
SKuzminov <SKuzminov@SKUZMINOV-PC>
2016-08-31 13:37:18 +1000

    golem encounter

--- Game/Program.cs
+++ Game/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Media;
 using System.Runtime.ExceptionServices;
@@ -13,6 +14,7 @@ namespace Game
     {
         static void Main(string[] args)
         {
+
             Console.WriteLine("****************************");
             Console.WriteLine("****************************");
             Console.WriteLine("****************************");
@@ -21,7 +23,15 @@ namespace Game
             Console.WriteLine("****************************");
             Console.WriteLine("****************************");
             Console.WriteLine("\nIntroduce yourself\n");
+            int LifePoints = 100;
+            if (LifePoints == 0)
+            {
+                Console.WriteLine("You are dead");
+                Console.WriteLine("GAME OVER");
+                Console.ReadLine();
+            }
             var playerName = "";
+
             while (true)
             {
             var input = Console.ReadLine();
@@ -49,15 +59,45 @@ namespace Game
             Console.WriteLine("****************************");
             Console.WriteLine("****************************");
             Console.WriteLine("\n\n"+ playerName + ", you are being teleported into battle");
-            Console.WriteLine("\nYou encounter stone golem");
-            Console.WriteLine("\nWhat are you going to do? Here are the spells you can use");
+            Console.WriteLine("\nYou encounter stone golem, what you are gonna do?");
             var abilityUsed = new classMage();
-            var validNumber = false;
-            while (!validNumber)
+            while (true)
             {
-                var enteredNumber = 0;
-                int.TryParse()
+                Console.WriteLine("Golem is getting closer...");
+                var input_ = Console.ReadLine();
+                input_ = input_.ToLower();
+                if (input_.Contains("felblast"))
+                {
+                    abilityUsed.felblast();
+                    Console.WriteLine("Golem explodes in a thousand pieces");
+                    break;
+                }
+                if (input_.Contains("icelance"))
+                {
+                    abilityUsed.icelance();
+                    Console.WriteLine(
+                        "Golem freezes for a little while, then after a short while, throws a piece of stone in your face. You take 1 damage");
+                    LifePoints--;
+                    Console.WriteLine("You have " + LifePoints + " life remaining");
+                }
+                else if (input_.Contains("fireball"))
+                {
+                    abilityUsed.fireball();
+                    Console.WriteLine("");
+                    LifePoints--;
+                    LifePoints--;
+                    Console.WriteLine("You have " + LifePoints + " life remaining");
+                }
+                else if (input_.Contains("blink"))
+                {
+                    abilityUsed.blink();
+                    Console.WriteLine("");
+                    Console.WriteLine("You have " + LifePoints + " life remaining");
+                }
+                
             }
+            //       else if (input_.Contains())
+        }
             //var input_ = Console.ReadLine();
             //int input_parsed;
             //if (int.TryParse(input_, out input_parsed))
@@ -67,5 +107,5 @@ namespace Game
             //else Console.WriteLine("Invalid input... Select a number");
 
         }
-    }
 }
+
--- Game/classMage.cs
+++ Game/classMage.cs
@@ -8,27 +8,36 @@ namespace Game
 {
     class classMage
     {
-        public void iceLance()
+        public void icelance()
         {
-            Console.WriteLine("You throw a piercing ice into target");
+            Console.WriteLine("\nYou throw a piercing ice into target\n");
         }
 
         public void fireball()
         {
-            Console.WriteLine("You blast a target with fireball");
+            Console.WriteLine("\nYou blast a target with fireball\n");
         }
 
-        public void felBlast()
+        public void felblast()
         {
-            Console.WriteLine("You inflame a target with demonic fire");
+            Console.WriteLine("\nYou inflame a target with demonic fire\n");
         }
 
         public void blink()
         {
-            Console.WriteLine("You are blinking in a short distance, avoiding the damage");
+            Console.WriteLine("\nYou are blinking in a short distance, avoiding the damage\n");
         }
 
-        enum Spellbook
+        public void showSpellbook()
+        {
+            Console.WriteLine("These are your spells..");
+            Console.WriteLine("Ice Lance");
+            Console.WriteLine("Fireball");
+            Console.WriteLine("Fel Blast");
+            Console.WriteLine("Blink");
+        }
+
+        public enum Spellbook
         {
             fireball = 1,
             iceLance = 2,
